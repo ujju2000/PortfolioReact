472ccef080c3fa4fa82675e17351ebc5466db777
Ujjwal Tarika <ujjutarika.35@gmail.com>
2023-12-31 10:44:49 +0530

    feature/connect contact form with emailid

--- src/App.js
+++ src/App.js
@@ -7,17 +7,20 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Projects from './components/Projects';
 import ClientWork from './components/ClientWork';
+import { NotificationContainer } from 'react-notifications';
+
 
 function App() {
   return (
       <div className="App text-slate-400">
-          <Header />
-          <Hero />
-          <Projects/>
-          <ClientWork />
-          <SkillsAndTech />
-          <Contact />
-          <Footer />
+            <Header />
+            <Hero />
+            <Projects/>
+            <ClientWork />
+            <SkillsAndTech />
+            <Contact />
+            <Footer />
+            <NotificationContainer />
       </div>
   );
 }
